64b403c5f2820ab9c7155a44302b5cc8c5627770
Divya-126 <dhotedivya126@gmail.com>
2026-06-23 20:29:59 +0530

    cleanup and README

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const controller = require("../controllers/productController");
 
-console.log(controller);
-
 router.get("/", controller.getProducts);
 
 module.exports = router;
